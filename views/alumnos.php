b457a5a1609c4f902fdbd16bcf393381d31adf05
ValentinoGasipi <valentino.gasipi@davinci.edu.ar>
2024-04-27 19:30:05 -0300

    Finalizado

--- views/alumnos.php
+++ views/alumnos.php
@@ -10,7 +10,7 @@
         <div class="col-md-6 d-flex justify-content-center align-items-center"> 
             <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
             <img src="img/valentino.png" class="efecto card-img-top">
-              <div class="card-header fw-bolder text-center text-danger">Valentino Gapisipi</div>
+              <div class="card-header fw-bolder text-center text-danger">Valentino Gasipi</div>
               <div class="card-body m-auto">
                 <h5 class="card-title">Programador y Diseñador</h5>
                 <p class="card-text">16 de Noviembre de 2004</p>
@@ -32,9 +32,9 @@
                 <p class="card-text">2 de Marzo de 2004</p>
                 <a href="mailto:yoel.lazarte@davinci.edu.ar" class="text-light">yoel.lazarte@davinci.edu.ar</a>
                 <section class="buttons mt-3">
-                  <a href="#" class="fa fa-github"></a>
+                  <a href="https://github.com/Nicko898" target="_blank" class="fa fa-github"></a>
                   <a href="#" class="fa fa-twitter"></a>
-                  <a href="#" class="fa fa-linkedin"></a>
+                  <a href="https://www.linkedin.com/in/yoel-lazarte/" target="_blank" class="fa fa-linkedin"></a>
                 </section>
               </div>
             </div>
